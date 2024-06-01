c1d88b34ba74b2ae5302a6512e3be33eb2353db4
Alice <you@example.com>
2024-06-01 19:11:47 +0900

    sixmonth command

--- sixmonth.py
+++ sixmonth.py
@@ -1,2 +1,46 @@
+import json
+import os.path
+import datetime
+
+import tabulate
+
+DELTA_THRESHOLD = -0.8
+
+
 def sixmonth():
-    print("sixmonth module")
+    path = os.path.join("./data", "expirations.json")
+    with open(path) as f:
+        expirations = json.load(f)
+    list_expirations(expirations)
+    print()
+    buy_put(expirations[5])
+
+
+def list_expirations(expirations: list[str]) -> None:
+    table = list()
+    for i in range(6):
+        date = format_expiration(expirations[i])
+        table.append((i + 1, date))
+    print(tabulate.tabulate(table, headers=("Month", "Expire date")))
+
+
+def buy_put(expiration: str) -> None:
+    dir_path = os.path.join("./data", expiration)
+    path = os.path.join(dir_path, "put.json")
+    with open(path) as f:
+        records = json.load(f)
+    for record in records:
+        delta = record["delta"]
+        if delta is None:
+            continue
+        if record["delta"] < DELTA_THRESHOLD:
+            date = format_expiration(expiration)
+            print("PUT to buy")
+            print("Expire at: ", date)
+            print("Strike price: ", record["strike"])
+            print("Delta: ", delta)
+            return
+
+
+def format_expiration(expiration: str) -> str:
+    return datetime.datetime.strptime(expiration, "%Y%m%d").strftime("%Y/%m/%d")
